b2ca92d6c2d96ec6892ab619d676afa940020a2c
ToshikiHata <toshiki_hata@f4samurai.jp>
2023-06-04 12:53:36 +0900

    同期プリミティブサンプル: SpinLock

---
+++ source/synchronization-primitives/SpinLockExample.cs
@@ -0,0 +1,45 @@
+namespace SynchronizationPrimitives.SpinLock
+{
+    public class SpinLockExample
+    {
+        private int _counter = 0;
+        private System.Threading.SpinLock _spinLock = new System.Threading.SpinLock();
+
+        public void Start()
+        {
+            Thread[] threads = new Thread[100];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                Thread thread = new Thread(new ThreadStart(Increment));
+                thread.Start();
+                threads[i] = thread;
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine($"counter:{_counter}");
+        }
+
+        private void Increment()
+        {
+            bool gotLock = false;
+            try
+            {
+                _spinLock.Enter(ref gotLock);
+                for (int i = 0; i < 10000; i++)
+                {
+                    _counter++;
+                }
+            }
+            finally
+            {
+                if (gotLock)
+                {
+                    _spinLock.Exit();
+                }
+            }
+        }
+    }
+}
