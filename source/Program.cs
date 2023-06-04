725f91ec6408df319f76e94e6648cd0340238172
ToshikiHata <toshiki_hata@f4samurai.jp>
2023-06-04 12:49:18 +0900

    呼び出し元のコード調整(この後各サンプルをコミット)

---
+++ source/Program.cs
@@ -0,0 +1,39 @@
+﻿using SynchronizationPrimitives.Semaphore;
+using SynchronizationPrimitives.Lock;
+using SynchronizationPrimitives.Mutex;
+using SynchronizationPrimitives.InterLocked;
+using SynchronizationPrimitives.RWLock;
+using SynchronizationPrimitives.SpinLock;
+using SynchronizationPrimitives.WaitHandle;
+using SynchronizationPrimitives.Monitor;
+using SynchronizationPrimitives.Barrier;
+
+new SemaphoreExample().Start();
+
+// new SemaphoreSlimExample().Start();
+
+// new LockExample().Start();
+
+// new MutexExample().Start();
+
+// new InterlockedExample().Start();
+
+// new ReaderWriterLockSlimExample().Start();
+
+// new SpinLockExample().Start();
+
+
+// new EventWaitHandleExample().Start();
+
+// new AutoResetEventExample().Start();
+
+// new ManualResetEventExample().Start();
+
+// new ManualResetEventSlimExample().Start();
+
+// new CountDownEventExample().Start();
+
+// new MonitorExample().Start();
+
+// new BarrierExample().Start();
+
